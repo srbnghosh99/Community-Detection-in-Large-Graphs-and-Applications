6f393c1dac92322cf502a5c57aaade42541f5f94
Shrabani Ghosh <srbn.ghosh99@gmail.com>
2023-05-20 18:07:55 -0400

    Update coauthor_net_generate.py

--- src/parseDBLP/coauthor_net_generate.py
+++ src/parseDBLP/coauthor_net_generate.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 20 17:58:59 2023
+
+@author: shrabanighosh
+"""
+
 import argparse
 import sys
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
@@ -10,46 +17,37 @@ from itertools import permutations,combinations
 
 
 def read_file(filename):
-  print("main",filename)
   df = pd.read_csv(filename,sep = ',')
-  pid_col = df[['info/authors/author/0/_pid','info/authors/author/1/_pid','info/authors/author/2/_pid','info/authors/author/3/_pid','info/authors/author/4/_pid','info/authors/author/5/_pid','info/authors/author/6/_pid','info/authors/author/7/_pid','info/authors/author/8/_pid','info/authors/author/9/_pid','info/authors/author/10/_pid','info/authors/author/11/_pid','info/authors/author/12/_pid','info/authors/author/13/_pid','info/authors/author/14/_pid','info/authors/author/15/_pid','info/authors/author/16/_pid','info/authors/author/17/_pid','info/authors/author/18/_pid','info/authors/author/19/_pid']]
-  pid_col.dropna(axis=1,how='all')
-  with open('coauthor_net.txt', 'w') as f:
-    f.write(f"A1 A2\n")
-    for i, row in pid_col.iterrows():
-        lis = []
-        for j, column in row.iteritems():
-          if(column is not np.nan):
-              lis.append(column)
-        # print((lis))
-        comb = combinations(lis, 2)
-        for i in list(comb):
-          # text = (i)
-          f.write(f"{i[0]} {i[1]}\n")
-  df1 = pd.read_csv("coauthor_net.txt",sep = " ")
-  df2 = df1['A1']
-  df3 = df1['A2']
-  frames = [df2, df3]
-  result = pd.concat(frames)
-  df4 = pd.DataFrame(result)
-  author_id = df4[0].unique().tolist()
-  lis = list(range(1,len(author_id)))
-  len(author_id),len(lis)
-  res = res = dict(zip(author_id, lis))
-  df1['A3'] = df1['A1'].map(res)
-  df1['A4'] = df1['A2'].map(res)
-  freq = df1.groupby(["A3", "A4"]).size().reset_index(name="Weight")
-  freq = freq.sort_values(by=['Weight'],ascending=False).reset_index()
-  freq= freq[["A3","A4","Weight"]]
-  freq['A4'] = freq[['A4']].astype(int)
-  # print(freq)
   new_file_name = filename.replace('.csv', '.edgelist') 
-  new_file_name = "coauthor_net_" + new_file_name
-  new_file_name = "data/" + new_file_name
-  print(new_file_name)
+  # new_file_name = "data/coauthor_net_" + new_file_name
+  # new_file_name_freq = "data/coauthor_net_freq_" + new_file_name
+
+  with open(new_file_name, 'w') as f:
+    f.write(f"Author1 Author2\n")
+    for i, row in df.iterrows():
+      # row['authors_name']
+      lis = row['authors_name'].split(',')
+      lis = [x.strip(' ') for x in lis]
+      # print(lis)
+      strippedText = str(lis).replace('[','').replace(']','').replace('\'','').replace('\"','')
+      # print(strippedText)
+      my_list = strippedText.split(",")
+      my_list = [x.strip(' ') for x in my_list]
+      my_list = [x.replace(' ', '_') for x in my_list] 
+      my_list = [i for i in my_list if i]
+      # print(my_list)
+      comb = combinations(my_list, 2)
+      for i in list(comb):
+        f.write(f"{i[0].strip()} {i[1].strip()}\n")
+  print("File Generated Co-author Network")
+  df1 = pd.read_csv(new_file_name,sep = " ")
+  freq = df1.groupby(["Author1", "Author2"]).size().reset_index(name="Weight")
+  freq = freq.sort_values(by=['Weight'],ascending=False).reset_index()
+  freq['Weight'] = freq[['Weight']].astype(int)
+  freq= freq[["Author1","Author2","Weight"]]
+  new_file_name = filename.replace('.csv', '_freq.edgelist') 
   freq.to_csv(new_file_name, sep = " ",header = False, index = False)
-
-
+  print("File Generated Co-author Frequncy Network")
 
 def parse_args():
    
