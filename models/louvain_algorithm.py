cfe45b3500ac93eeff6ca3402cc4f74a607754c8
Shrabani Ghosh <srbn.ghosh99@gmail.com>
2023-06-13 23:21:08 -0400

    Update louvain_algorithm.py

--- models/louvain_algorithm.py
+++ models/louvain_algorithm.py
@@ -1,3 +1,5 @@
+
+
 from community import community_louvain
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
@@ -5,40 +7,65 @@ import networkx as nx
 import json
 import csv
 import pandas as pd
+import os
+import csv
+import argparse
+import sys
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
-# load the karate club graph
-G = nx.karate_club_graph()
-G = nx.read_edgelist("merge_file_freq.edgelist", nodetype=str, data=(("weight", int),))
 
+def louvain_algo(inputpath,outputpath):
+    # load the karate club graph
+    G = nx.karate_club_graph()
+    # G = nx.read_edgelist("merge_file_freq.edgelist", nodetype=str, data=(("weight", int),))
+    G = nx.read_edgelist(inputpath, nodetype=str, data=(("weight", int),))
 
-# compute the best partition
-partition = community_louvain.best_partition(G)
+    # compute the best partition
+    partition = community_louvain.best_partition(G)
 
-# print(partition)
-# file = 'louvain_authors.json' 
-# json.dump( partition, open( "data/myfile.json", 'w' ) )
-headerList = ['author','community']
-with open('louvain_author_community.csv', 'w') as f:
+    # print(partition)
+    # file = 'louvain_authors.json' 
+    # json.dump( partition, open( "data/myfile.json", 'w' ) )
+    headerList = ['author','community']
+    with open('louvain_author_community.csv', 'w') as f:
+        
+        # dw = csv.DictWriter(f, delimiter=',', 
+                            # fieldnames=headerList)
+        for key in partition.keys():
+            f.write("%s,%s\n"%(key,partition[key]))
+    df = pd.read_csv('test.csv')
+
+    df.columns = ['author','community']
+    df.to_csv("test.csv")
+    # with open(file, 'w') as f: 
+    #     json.dump(partition, f)
+    # draw the graph
+    # pos = nx.spring_layout(G)
+    # # color the nodes according to their partition
+    # cmap = cm.get_cmap('viridis', max(partition.values()) + 1)
+
+    # nx.draw_networkx_nodes(G, pos, partition.keys(), node_size=40,
+    #                        cmap=cmap, node_color=list(partition.values()))
+    # nx.draw_networkx_edges(G, pos, alpha=0.5)
+    # plt.show()
+
+def parse_args():
+   
+    parser = argparse.ArgumentParser(description="Read File")
+
+    parser.add_argument("--inputdirectory",type = str)
+    parser.add_argument("--outputdirectory",type = str)
     
-    # dw = csv.DictWriter(f, delimiter=',', 
-                        # fieldnames=headerList)
-    for key in partition.keys():
-        f.write("%s,%s\n"%(key,partition[key]))
-df = pd.read_csv('test.csv')
+    return parser.parse_args()
 
-df.columns = ['author','community']
-df.to_csv("test.csv")
-# with open(file, 'w') as f: 
-#     json.dump(partition, f)
-# draw the graph
-# pos = nx.spring_layout(G)
-# # color the nodes according to their partition
-# cmap = cm.get_cmap('viridis', max(partition.values()) + 1)
+def main():
+    inputs=parse_args()
+    print(inputs.inputdirectory)
+    print(inputs.outputdirectory)
+    louvain_algo(inputs.inputdirectory,inputs.outputdirectory)
+  
 
-# nx.draw_networkx_nodes(G, pos, partition.keys(), node_size=40,
-#                        cmap=cmap, node_color=list(partition.values()))
-# nx.draw_networkx_edges(G, pos, alpha=0.5)
-# plt.show()
-
+if __name__ == '__main__':
+    main()
 
 
