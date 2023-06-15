3b7e5d6f6d868639312311e7f0078b51a86d795d
Erik Saule <esaule@uncc.edu>
2023-06-15 15:39:46 -0400

    toward flask template app

---
+++ src/webserver/app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+import networkx as nx
+from datetime import datetime
+
+app = Flask(__name__)
+G = nx.Graph()
+
+def load_graph(edgelist_filename: str):
+    print("Starting to load graph at =", datetime.now().strftime("%H:%M:%S"))
+    G = nx.read_edgelist(edgelist_filename, delimiter=" ", data=(("weight", int),))
+    print("Finished loading graph at =", datetime.now().strftime("%H:%M:%S"))
+
+@app.route('/vertices')
+def vert():
+    #TODO should return a JSON list of all vertices
+    pass
+
+@app.route('/neighborsof/<id>')
+def neighbor(id: str):
+    #TODO should return a JSON list of all neighbors of that vertex
+    pass
+
+@app.route('/')
+def index():
+    return 'Web App with Python Flask!'
+
+
+
+#load_graph('data/dblp-coauthor.edgelist')
+load_graph('data/sample_HCI_coauthornet.edgelist')
+app.run(host='0.0.0.0')
