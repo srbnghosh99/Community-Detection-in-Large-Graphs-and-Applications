e35d0ab9702aaf77e43538e4e6286e15ef11e347
Erik Saule <esaule@uncc.edu>
2023-06-15 15:43:18 -0400

    list of vertices

--- src/webserver/app.py
+++ src/webserver/app.py
@@ -1,19 +1,21 @@
 from flask import Flask
+from flask import jsonify
 import networkx as nx
 from datetime import datetime
+
 
 app = Flask(__name__)
 G = nx.Graph()
 
 def load_graph(edgelist_filename: str):
+    global G
     print("Starting to load graph at =", datetime.now().strftime("%H:%M:%S"))
     G = nx.read_edgelist(edgelist_filename, delimiter=" ", data=(("weight", int),))
     print("Finished loading graph at =", datetime.now().strftime("%H:%M:%S"))
 
 @app.route('/vertices')
 def vert():
-    #TODO should return a JSON list of all vertices
-    ret=G.nodes
+    ret=list(G.nodes)
     return jsonify(ret)
 
 @app.route('/neighborsof/<id>')
