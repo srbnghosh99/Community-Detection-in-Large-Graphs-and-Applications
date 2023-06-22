308ed6ddde7c031f51c20db088f21544143a405a
Erik Saule <esaule@uncc.edu>
2023-06-22 15:12:23 -0400

    flask reverse map

--- src/webserver/app.py
+++ src/webserver/app.py
@@ -7,7 +7,9 @@ import csv
 app = Flask(__name__)
 G = nx.Graph()
 
-nonoverlappingcommunity={}
+nonoverlappingcommunity_vertexmap={}  #nonoverlappingcommunity_vertexmap["community"]["vertexid"]:int
+nonoverlappingcommunity_communitymap={} #nonoverlappingcommunity_vertexmap["community"][communityid]:list:str
+
 
 def load_graph(edgelist_filename: str):
     global G
@@ -33,7 +35,7 @@ def community_of(community_name:str, vertex_id: str):
     data = {}
 
     try:
-        data[vertex_id] = nonoverlappingcommunity[community_name][vertex_id]
+        data[vertex_id] = nonoverlappingcommunity_vertexmap[community_name][vertex_id]
         ret["status"] = "OK"
         ret["data"] = data
     except:
@@ -43,14 +45,31 @@ def community_of(community_name:str, vertex_id: str):
 
 @app.route('/community/<community_name>/all/<int:community_id>')
 def community_all(community_name:str, community_id:int):
-    #TODO should return the ids of all vertices in community_id from community structure community_name
-    pass
+    ret={}
+
+    try:
+        communityset = nonoverlappingcommunity_communitymap[community_name][community_id]
+        ret["data"] = list(communityset) #set() are not JSON serializable in python
+        ret["status"] = "OK"
+    except:
+        ret["status"] = "KO"
+
+    return jsonify(ret)
 
 
 @app.route('/')
 def index():
     return 'Web App with Python Flask!'
 
+def build_nonoverlappingcommunitymap_fromvertexmap(commname:str):
+    reversemap = {}
+    vertexmap = nonoverlappingcommunity_vertexmap[commname]
+    for vertexid in vertexmap:
+        commid = vertexmap[vertexid]
+        if commid not in reversemap:
+            reversemap[commid] = set()
+        reversemap[commid].add(vertexid)
+    nonoverlappingcommunity_communitymap[commname]=reversemap
 
 
 # loads a community file. Assume that the format of the file is:
@@ -64,7 +83,8 @@ def load_community_nonoverlapping(commname:str, filename:str):
         reader = csv.DictReader(csvfile, delimiter=' ')
         for row in reader:
             comm[row['Vertex']] = int(row['Community'])
-        nonoverlappingcommunity[commname] = comm
+        nonoverlappingcommunity_vertexmap[commname] = comm
+        build_nonoverlappingcommunitymap_fromvertexmap(commname)
 
 
 #load_graph('data/dblp-coauthor.edgelist')
