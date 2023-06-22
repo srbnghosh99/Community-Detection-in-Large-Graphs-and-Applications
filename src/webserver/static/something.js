ec0c2038ffd5fa03c00cfe0f6087812f3464b09b
Erik Saule <esaule@uncc.edu>
2023-06-22 15:42:31 -0400

    some queries

--- src/webserver/static/something.js
+++ src/webserver/static/something.js
@@ -3,6 +3,13 @@ function test() {
     console.log("hello");
     const myp = document.querySelector("p");
     myp.textContent = "Hello world!";
+    const loading = async() => {
+	const response = await fetch('/communities');
+	const myjson = await response.json();
+	myp.textContent = myjson["status"];
+    }
+    loading();
+    
 }
 
 window.addEventListener("load", test);
