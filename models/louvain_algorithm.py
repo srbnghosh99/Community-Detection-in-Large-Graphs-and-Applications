e8ce12fc44206ff2ae33baac08908db6f5b29e2a
Shrabani Ghosh <srbn.ghosh99@gmail.com>
2024-07-07 09:09:47 -0700

    Update louvain_algorithm.py

--- models/louvain_algorithm.py
+++ models/louvain_algorithm.py
@@ -13,46 +13,24 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 
 def louvain_algo(inputfile,outputfile):
-    # load the karate club graph
-    #G = nx.karate_club_graph()
-    # G = nx.read_edgelist("merge_file_freq.edgelist", nodetype=str, data=(("weight", int),))
-    G = nx.read_edgelist(inputfile, nodetype=str, data=(("weight", int),))
-
+    G=nx.read_weighted_edgelist(inputfile)
     # compute the best partition
     partition = community_louvain.best_partition(G)
-
     # print(partition)
-    # file = 'louvain_authors.json' 
-    # json.dump( partition, open( "data/myfile.json", 'w' ) )
-    headerList = ['author','community']
+
     with open('test.csv', 'w') as f:
         
         # dw = csv.DictWriter(f, delimiter=',', 
                             # fieldnames=headerList)
         for key in partition.keys():
             f.write("%s,%s\n"%(key,partition[key]))
-    df = pd.read_csv('test.csv')
+    column_names = ['Node','Community']
+    df = pd.read_csv('test.csv',names = column_names)
+    df = df.sort_values(by=['Node'])
+    print(df)
 
-    df.columns = ['author','community']
-    df.to_csv(outputfile, index = False, sep = ' ')
-    df = df.sort_values(by=['author'])
-    df1 = pd.read_csv("sorted_dblp_author_nameid.csv", sep = ' ')
-    author_dic = dict(zip(df1.Id, df1.Author))
-    df['author'] = df['author'].map(author_dic)
-    df.community.value_counts().rename_axis('community').reset_index(name='counts').to_csv("louvain_comm_value_counts.csv", sep = ' ', index = False)
-    
+    df.to_csv(outputfile, sep = ',',index = False)
 
-# with open(file, 'w') as f: 
-    #     json.dump(partition, f)
-    # draw the graph
-    # pos = nx.spring_layout(G)
-    # # color the nodes according to their partition
-    # cmap = cm.get_cmap('viridis', max(partition.values()) + 1)
-
-    # nx.draw_networkx_nodes(G, pos, partition.keys(), node_size=40,
-    #                        cmap=cmap, node_color=list(partition.values()))
-    # nx.draw_networkx_edges(G, pos, alpha=0.5)
-    # plt.show()
 
 def parse_args():
    
@@ -60,7 +38,6 @@ def parse_args():
 
     parser.add_argument("--inputfilename",type = str)
     parser.add_argument("--outputfilename",type = str)
-    
     return parser.parse_args()
 
 def main():
@@ -72,5 +49,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
