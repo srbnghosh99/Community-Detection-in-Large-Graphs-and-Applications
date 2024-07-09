3e594978513e0dd1e2d429c7430f99568692ce38
Shrabani Ghosh <srbn.ghosh99@gmail.com>
2024-07-08 17:13:47 -0700

    Update process_rawfile.py

--- Use Cases/Recommendatin_System/process_rawfile.py
+++ Use Cases/Recommendatin_System/process_rawfile.py
@@ -30,6 +30,21 @@ def raw_file_read(directory,dataset):
     trustnet_df.to_csv(csv_fname,index = False)
     print("done")
 
+    ### Renumber nodes
+    G = nx.read_edgelist(csv_fname,delimiter=' ', nodetype=int)
+    print(G.number_of_nodes(), G.number_of_edges(), len(sorted(G.nodes())))
+    nodes_with_less_than_two_edges = [node for node, degree in G.degree() if degree < 2]
+    print('number of user with less than two trustors',len(nodes_with_less_than_two_edges))
+    G.remove_nodes_from(nodes_with_less_than_two_edges)
+    df_edges = nx.to_pandas_edgelist(G)
+    #df_edges.to_csv("/Users/shrabanighosh/Downloads/data/trust_prediction/ciao/preprocessed_ciao_edges.csv",index=False, header=False)
+    remaining_nodes = sorted(list(G.nodes()))
+    mapping = {node: new_number + 1 for new_number, node in enumerate(remaining_nodes)}
+    G_renumbered = nx.relabel_nodes(G, mapping)
+    # Write the renumbered graph to a file
+    output_file = "renumbered_graph_" + dataset + ".csv"
+    nx.write_edgelist(G_renumbered, output_file, delimiter=' ', data=False)
+
 
 
 def parse_args():
