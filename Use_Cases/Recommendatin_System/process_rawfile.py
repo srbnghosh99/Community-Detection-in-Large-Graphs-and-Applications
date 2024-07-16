97ee661eb40924991b637866b056df385def0469
Shrabani Ghosh <srbn.ghosh99@gmail.com>
2024-07-16 10:44:48 -0400

    Renamed directory old-directory-name to new-directory-name

--- Use_Cases/Recommendatin_System/process_rawfile.py
+++ Use_Cases/Recommendatin_System/process_rawfile.py
@@ -5,6 +5,7 @@ import argparse
 import sys
 import re
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+import networkx as nx
 
 # READ MAT FILES
 #  Download the mat files from this link --> "https://www.cse.msu.edu/~tangjili/datasetcode/truststudy.htm"
@@ -23,18 +24,19 @@ def raw_file_read(directory,dataset):
     mat_fname = pjoin(directory,dataset, 'trustnetwork.mat')
     mat_contents = sio.loadmat(mat_fname)
     trustnetwork_array = mat_contents['trustnetwork']
-    trustnet_df = pd.DataFrame(trustnetwork_array, columns=['user1', 'user2'])
+#    trustnet_df = pd.DataFrame(trustnetwork_array, columns=['user1', 'user2'])
+    trustnet_df = pd.DataFrame(trustnetwork_array)
     csv_fname = mat_fname.replace(".mat", ".csv")
     print(rating_df)
     print(trustnet_df)
-    trustnet_df.to_csv(csv_fname,index = False)
+    trustnet_df.to_csv(csv_fname,sep = ' ' ,index = False)
     
 
     ### Renumber nodes
     G = nx.read_edgelist(csv_fname,delimiter=' ', nodetype=int)
-    print(G.number_of_nodes(), G.number_of_edges(), len(sorted(G.nodes())))
+    print("Number of nodes",G.number_of_nodes(), "Number of edges",G.number_of_edges())
     nodes_with_less_than_two_edges = [node for node, degree in G.degree() if degree < 2]
-    print('number of user with less than two trustors',len(nodes_with_less_than_two_edges))
+    print('number of users with less than two trustors',len(nodes_with_less_than_two_edges))
     G.remove_nodes_from(nodes_with_less_than_two_edges)
     df_edges = nx.to_pandas_edgelist(G)
     #df_edges.to_csv("/Users/shrabanighosh/Downloads/data/trust_prediction/ciao/preprocessed_ciao_edges.csv",index=False, header=False)
@@ -43,8 +45,10 @@ def raw_file_read(directory,dataset):
     G_renumbered = nx.relabel_nodes(G, mapping)
     # Write the renumbered graph to a file
     output_file = "renumbered_graph_" + dataset + ".csv"
+    filename = pjoin(directory,dataset, output_file)
+    
     nx.write_edgelist(G_renumbered, output_file, delimiter=' ', data=False)
-    print("done")
+    print("Done")
 
 
 
@@ -62,3 +66,8 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+python3 /Users/shrabanighosh/Downloads/data/jsontocsv.py --inputfilename /recommendation_system/ciao/ego_splitting_res2.5_min10.json --outputfilename recommendation_system/ciao/ego_splitting_res2.5_min10.csv
+
+python3 /Users/shrabanighosh/Downloads/data/recommendation_system/overlapping_processing.py --inputfilename
