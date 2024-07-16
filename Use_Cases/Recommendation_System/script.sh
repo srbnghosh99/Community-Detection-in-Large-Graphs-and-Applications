b2bd3380af6fc98448183b295a8cb256be557ff7
Shrabani Ghosh <srbn.ghosh99@gmail.com>
2024-07-16 17:33:41 -0400

    update bashfile

--- Use_Cases/Recommendation_System/script.sh
+++ Use_Cases/Recommendation_System/script.sh
@@ -21,8 +21,8 @@ python3 ../jsontocsv.py --dataset ciao --inputfilename ego_splitting_res2.5_min1
 
 python3 social_recommendation_system.py --dataset ciao --graphfile renumbered_graph_ciao.csv --cdfile  ego_splitting_res2.5_min10.csv --outdir ego_splitting_res2.5_min10/ --overlap overlapping
 
-python3 ../trust_prediction/node_propensity.py --dataset ciao --inDirectory ego_splitting_res2.5_min10 --outDirectory propensity_res2.5_min10
+python3 ../trust_prediction/create_node_propensity.py --dataset ciao --inDirectory ego_splitting_res2.5_min10 --outDirectory propensity_res2.5_min10
 
-python3 node_propensity.py --dataset ciao --cdfile ego_splitting_res2.5_min10.csv --inputdir propensity_res2.5_min10 --outputdir propensity_res2.5_min10 --overlap overlapping
-
+python3 generate_dataframe_from_propensite.py --dataset ciao --cdfile ego_splitting_res2.5_min10.csv --inputdir propensity_res2.5_min10 --outputdir propensity_res2.5_min10 --overlap overlapping
+#
 python3 rating_prediction.py --dataset ciao --trustnet renumbered_graph_ciao.csv --ratingfile rating.csv --communityfile ego_splitting_res2.5_min10.csv --inputdir /recommendation_system/ciao --output_dir propensity_res2.5_min10/ --overlap overlapping
