2e53f2ab26d63c3d0bc8ed9c0b71d3e8181c8570
Shrabani Ghosh <srbn.ghosh99@gmail.com>
2024-07-19 17:36:13 -0400

    Update script.sh

--- Use_Cases/Trust_Prediction/script.sh
+++ Use_Cases/Trust_Prediction/script.sh
@@ -16,12 +16,12 @@ unzip ciao.zip
 python3 preprocess.py --dataset ciao
 
 #python3 ../jsontocsv.py --dataset ciao --inputfilename ego_splitting_res2.5_min10.json
-#
-python3 subgraph_create.py --dataset ciao --graphfile renumbered_graph_epinions.csv --inputfilename louvain_ciao.csv --overlapping nonoverlapping --outdirectory louvain
-#
-#python3 create_node_propensity.py --inDirectory /Users/shrabanighosh/Downloads/data/trust_prediction/epinions/louvain --outDirectory /Users/shrabanighosh/Downloads/data/trust_prediction/epinions/propensity_subgraph_louvain
-#
-#python3 find_center_of_communities.py --directory /Users/shrabanighosh/Downloads/data/trust_prediction/epinions/propensity_subgraph_louvain
-#
-#python3 trust_prediction.py --graphfile renumbered_graph_ciao.csv --communityfile community_clusters/spectral_ciao_c10.csv --community_center propensity_subgraph_spectral/centerclusters.csv --ratingfile ciao_rating.csv --overlap nonoverlap
-#
+
+python3 subgraph_create.py --dataset ciao --graphfile renumbered_graph_ciao.csv --inputfilename louvain_ciao.csv --overlapping nonoverlapping --outdirectory louvain
+
+python3 create_node_propensity.py --dataset ciao --inDirectory louvain --outDirectory propensity_subgraph_louvain
+
+python3 find_center_of_communities.py --dataset ciao --directory propensity_subgraph_louvain
+
+python3 trust_prediction.py --dataset ciao --graphfile renumbered_graph_ciao.csv --communityfile louvain_ciao.csv --community_center propensity_subgraph_louvain/centerclusters.csv --ratingfile rating.csv --overlap nonoverlap
+
