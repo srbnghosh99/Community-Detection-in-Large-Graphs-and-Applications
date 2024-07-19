d66ac53ce19e4030c03a8762c915a35912fafe8c
Shrabani Ghosh <srbn.ghosh99@gmail.com>
2024-07-19 14:51:39 -0400

    Update plot_community_metrices.py

--- plot_community_metrices.py
+++ plot_community_metrices.py
@@ -17,6 +17,8 @@ def plot_metrices(directory_path):
     # Read each CSV file into a DataFrame
     dataframes = [pd.read_csv(os.path.join(directory_path, file)) for file in csv_files]
 
+    # print(dataframes)
+
     # Extract 'cluster_coeff_lis' column values from each DataFrame
     cluster_coeff_lis_values = [df['cluster_coeff_lis'].dropna().tolist() for df in dataframes]
 
@@ -30,9 +32,10 @@ def plot_metrices(directory_path):
 
     normalized_cut_values = [df['normalized_cut_lis'].dropna().tolist() for df in dataframes]
 
-    num_communities = [df['Number of communities'][0] for df in dataframes]
-    modularity = [df['Modularity'][0] for df in dataframes]
-    avg_clustering_coefficient = [df['overall_avg_clustering_coefficient'][0] for df in dataframes]
+    num_communities = [df['Number of communities'].dropna().iloc[0] for df in dataframes]
+    modularity = [df['Modularity'].dropna().iloc[0] for df in dataframes]
+    avg_clustering_coefficient = [df['overall_avg_clustering_coefficient'].dropna().iloc[0] for df in dataframes]
+    # print(cluster_coeff_lis_values)
 
 
     # Function to calculate statistical summary
@@ -51,15 +54,17 @@ def plot_metrices(directory_path):
         # Calculate stats for each dataset
         for i, values in enumerate(lis_values):
             stats = calculate_stats(values)
-            print(f"Stats for Dataset {i+1}: Mean={stats[0]}, Median={stats[1]}, 75th Percentile={stats[2]}, 90th Percentile={stats[3]}")
+            # print(f"Stats for Dataset {i+1}: Mean={stats[0]}, Median={stats[1]}, 75th Percentile={stats[2]}, 90th Percentile={stats[3]}")
         
 
 
         lis_values_norm = [normalize(np.array(values)) for values in lis_values]
 
+
         # Apply log transformation to the data
         values_log = [np.log1p(values) for values in lis_values]
-        return values_log
+        return lis_values_norm
+        # return lis_values
 
 
     cluster_coeff_values_log = iterate_dataset(cluster_coeff_lis_values)
@@ -71,89 +76,75 @@ def plot_metrices(directory_path):
 
 
 
-    # Plotting
-    fig, axs = plt.subplots(2, 3, figsize=(18, 12))
-    fig.suptitle('Log-Transformed Box Plots of Various Metrics of Non-Overlapping Community Detection Methods')
+    # Create a grid of subplots (3 rows by 3 columns)
+    fig, axs = plt.subplots(3, 3, figsize=(18, 12))
 
+    # Box plots
     sns.boxplot(data=cluster_coeff_values_log, ax=axs[0, 0])
     axs[0, 0].set_title('Clustering Coefficient')
     axs[0, 0].set_xlabel('Clustering Methods')
-    axs[0, 0].set_ylabel('Log-Transformed Values')
+    axs[0, 0].set_ylabel('Values')
     axs[0, 0].set_xticklabels(dataset_names)
 
     sns.boxplot(data=density_values_log, ax=axs[0, 1])
     axs[0, 1].set_title('Density')
     axs[0, 1].set_xlabel('Clustering Methods')
-    axs[0, 1].set_ylabel('Log-Transformed Values')
+    axs[0, 1].set_ylabel('Values')
     axs[0, 1].set_xticklabels(dataset_names)
 
     sns.boxplot(data=conductance_values_log, ax=axs[0, 2])
     axs[0, 2].set_title('Conductance')
     axs[0, 2].set_xlabel('Clustering Methods')
-    axs[0, 2].set_ylabel('Log-Transformed Values')
+    axs[0, 2].set_ylabel('Values')
     axs[0, 2].set_xticklabels(dataset_names)
 
     sns.boxplot(data=coverage_values_log, ax=axs[1, 0])
     axs[1, 0].set_title('Coverage')
     axs[1, 0].set_xlabel('Clustering Methods')
-    axs[1, 0].set_ylabel('Log-Transformed Values')
+    axs[1, 0].set_ylabel('Values')
     axs[1, 0].set_xticklabels(dataset_names)
 
     sns.boxplot(data=edge_cut_values_log, ax=axs[1, 1])
     axs[1, 1].set_title('Edge Cut')
     axs[1, 1].set_xlabel('Clustering Methods')
-    axs[1, 1].set_ylabel('Log-Transformed Values')
+    axs[1, 1].set_ylabel('Values')
     axs[1, 1].set_xticklabels(dataset_names)
 
     sns.boxplot(data=normalized_cut_values_log, ax=axs[1, 2])
     axs[1, 2].set_title('Normalized Cut')
     axs[1, 2].set_xlabel('Clustering Methods')
-    axs[1, 2].set_ylabel('Log-Transformed Values')
+    axs[1, 2].set_ylabel('Values')
     axs[1, 2].set_xticklabels(dataset_names)
 
-    plt.tight_layout(rect=[0, 0, 1, 0.96])
+    # Bar plots
+    indices = range(len(dataset_names))
 
-    # Save the figure
-    plt.savefig(directory_path+'/log_transformed_box_plots.png')
+    axs[2, 0].bar(indices, num_communities, color='skyblue')
+    axs[2, 0].set_title('Number of Communities')
+    axs[2, 0].set_xlabel('Clustering Methods')
+    axs[2, 0].set_ylabel('Values')
+    axs[2, 0].set_xticks(indices)
+    axs[2, 0].set_xticklabels(dataset_names, rotation=45, ha='right')
 
-    # plt.show()
+    axs[2, 1].bar(indices, modularity, color='lightgreen')
+    axs[2, 1].set_title('Modularity')
+    axs[2, 1].set_xlabel('Clustering Methods')
+    axs[2, 1].set_ylabel('Values')
+    axs[2, 1].set_xticks(indices)
+    axs[2, 1].set_xticklabels(dataset_names, rotation=45, ha='right')
 
-    # Indices for bar positions
-    indices = range(len(num_communities))
-
-    # Create subplots
-    fig, axs = plt.subplots(1, 3, figsize=(15, 5))
-    fig.suptitle('Various Metrics of Non-Overlapping Community Detection Methods')
-
-    # Plot num_communities as bar plot
-    axs[0].bar(indices, num_communities, color='skyblue')
-    axs[0].set_title('Number of Communities')
-    axs[0].set_xlabel('Index')
-    axs[0].set_ylabel('Number of Communities')
-    axs[0].set_xticks(indices)
-    axs[0].set_xticklabels(dataset_names)
-
-    # Plot modularity as bar plot
-    axs[1].bar(indices, modularity, color='lightgreen')
-    axs[1].set_title('Modularity')
-    axs[1].set_xlabel('Index')
-    axs[1].set_ylabel('Modularity')
-    axs[1].set_xticks(indices)
-    axs[1].set_xticklabels(dataset_names)
-
-    # Plot clustering_coefficient as bar plot
-    axs[2].bar(indices, avg_clustering_coefficient, color='lightcoral')
-    axs[2].set_title('Clustering Coefficient')
-    axs[2].set_xlabel('Index')
-    axs[2].set_ylabel('Clustering Coefficient')
-    axs[2].set_xticks(indices)
-    axs[2].set_xticklabels(dataset_names)
+    axs[2, 2].bar(indices, avg_clustering_coefficient, color='lightcoral')
+    axs[2, 2].set_title('Clustering Coefficient')
+    axs[2, 2].set_xlabel('Clustering Methods')
+    axs[2, 2].set_ylabel('Values')
+    axs[2, 2].set_xticks(indices)
+    axs[2, 2].set_xticklabels(dataset_names, rotation=45, ha='right')
 
     # Adjust layout
     plt.tight_layout()
-
+    # plt.show()
     # Save the figure
-    plt.savefig(directory_path +'/metrics_barplots.png')
+    plt.savefig(directory_path +'/metrics_plots.png')
 
     # Show plot
     # plt.show()
@@ -173,7 +164,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# command
-#  python3 /Users/shrabanighosh/Downloads/data/recommendation_system/plot_community_metrices.py --directory_path /Users/shrabanighosh/Downloads/data/recommendation_system/ciao/community_metrices
