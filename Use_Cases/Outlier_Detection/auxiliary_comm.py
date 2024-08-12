f7d5ec7e8c1f43b1c20aacaf01119c60d4ba181e
Shrabani Ghosh <srbn.ghosh99@gmail.com>
2024-08-12 00:59:11 -0400

    Update auxiliary_comm.py

--- Use_Cases/Outlier_Detection/auxiliary_comm.py
+++ Use_Cases/Outlier_Detection/auxiliary_comm.py
@@ -8,42 +8,83 @@ import seaborn as sns
 from pathlib import Path
 import csv
 from tqdm import tqdm
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+import argparse
 
-
-G = nx.read_edgelist("/Users/shrabanighosh/Downloads/data/outlier_detection/DGraph_fin_header_false.edgelist",delimiter=' ',nodetype=int)
-print(G.number_of_nodes(), G.number_of_edges())
-
-finan_comm = pd.read_csv("/Users/shrabanighosh/Downloads/data/outlier_detection/assignments/DGraph_fin_header_false_label_prop.csv",sep = ',')
-# finan_comm.columns = ['Node','Community']
-# finan_comm.sort_values(by=['Community'], key=lambda x: x.astype(int)).reset_index()
-
-finan_comm = finan_comm.sort_values(by=['Node'], key=lambda x: x.astype(int)).reset_index()
-print(finan_comm)
-
-
-count = 0
-auxiliary_communities = []
 
 def are_all_same(lst):
     return all(x == lst[0] for x in lst)
 
-total_nodes = len(G.nodes())
-progress_bar = tqdm(total=total_nodes, desc="Processing Nodes", unit="node")
+def find_auxiliary_communities(graph_file,community_file,output):
 
-for node in G.nodes(data=False):
-    # print(node)
-    node_comm = finan_comm.loc[finan_comm['Node'] == node, 'Community'].iloc[0]
-    # print(finan_comm.loc[finan_comm['Node'] == node])
-    # print("node: ",node, node_comm) 
-    # break
-    neighbors = list(G.neighbors(node))
-    df = finan_comm[finan_comm['Node'].isin(neighbors)]
-    comm = df["Community"].tolist()
-    result_different = are_all_same(comm)
-    if result_different == False:
-        auxiliary_communities.append(node_comm)
-    progress_bar.update()
-progress_bar.close()
-print("DOne")
-print(len(auxiliary_communities))
+    G = nx.read_edgelist(graph_file,delimiter=' ',nodetype=int)
+    print(G.number_of_nodes(), G.number_of_edges())
 
+    #finan_comm = pd.read_csv("/Users/shrabanighosh/Downloads/data/outlier_detection/assignments/finan/louvain_financial_data.csv",sep = ' ')
+    df_comm = pd.read_csv(community_file,sep = ',')
+    # finan_comm.columns = ['Node','Community']
+    # finan_comm.sort_values(by=['Community'], key=lambda x: x.astype(int)).reset_index()
+    #print(finan_comm)
+    df_comm = df_comm.sort_values(by=['Node'], key=lambda x: x.astype(int)).reset_index()
+    df_comm = df_comm[['Node','Community']]
+    print(df_comm)
+
+    count = 0
+    auxiliary_communities = []
+
+    node_to_community = df_comm.set_index('Node')['Community'].to_dict()
+    nodelis = []
+    community_nodes = df_comm.groupby('Community')['Node'].apply(list).to_dict()
+    auxiliary_communities = []
+    inc = 0
+    # Initialize auxiliary communities set
+    auxiliary_communities = {}
+    auxiliary_community_counter = 1
+
+
+    total_communities = len(community_nodes)
+    progress_bar = tqdm(total=total_communities, desc="Processing Nodes", unit="node")
+    for community, nodes in community_nodes.items():
+        for u in nodes:
+            neighbors = list(G.neighbors(u))
+            df = df_comm[df_comm['Node'].isin(neighbors)]
+            comm = df["Community"].tolist()
+            unique_set = set(comm)
+            unique_comms = list(unique_set)
+            result_different = are_all_same(unique_comms)
+            if result_different == False:
+                nodelis.append(u)
+                aux_community_id = f'AUX_{auxiliary_community_counter}'
+                auxiliary_community_counter += 1
+                for v in neighbors:
+                    if (node_to_community[u] != node_to_community[v]) and (u != v):
+                        auxiliary_communities[aux_community_id] = [u,v]
+            progress_bar.update()
+        progress_bar.close()
+
+    combined_communities = {**community_nodes, **auxiliary_communities}
+    print(combined_communities)
+
+    column_names = ['Community','Node']
+    df = pd.DataFrame(list(combined_communities.items()), columns=column_names)
+
+    print(df)
+    df.to_csv(output, index = False)
+
+def parse_args():
+   
+    parser = argparse.ArgumentParser(description="Read File")
+    
+    parser.add_argument("--graph_file",type = str)
+    parser.add_argument("--community_file",type = str)
+    parser.add_argument("--outputfile",type = str)
+    return parser.parse_args()
+
+def main():
+    inputs=parse_args()
+    print(inputs.graph_file)
+    print(inputs.community_file)
+    find_auxiliary_communities(inputs.graph_file,inputs.community_file,inputs.outputfile)
+  
+if __name__ == '__main__':
+    main()
