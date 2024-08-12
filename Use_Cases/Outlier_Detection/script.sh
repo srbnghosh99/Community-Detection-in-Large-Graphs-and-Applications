1483a0b7db8819ce501ccd0349de88c7b43d2615
Shrabani Ghosh <srbn.ghosh99@gmail.com>
2024-08-12 01:06:19 -0400

    Update script.sh

--- Use_Cases/Outlier_Detection/script.sh
+++ Use_Cases/Outlier_Detection/script.sh
@@ -1,3 +1,11 @@
+mkdir YelpHotel
+
+cd YelpHotel
+
+curl -L -o YelpHotel.mat https://raw.githubusercontent.com/QZ-WANG/ACT/main/datasets/YelpHotel/YelpHotel.mat
+
+python3 yelp.py
+
 python3 auxiliary_comm.py --graph_file YelpHotel/graph_edges.csv --community_file YelpHotel/spectral.csv --outputfile YelpHotel/spectral_auxiliary_communities1.csv
 
 python3 feature_extraction.py --graph_file YelpHotel/graph_edges.csv --auxiliary_community_file YelpHotel/spectral_auxiliary_communities1.csv --outputfile YelpHotel/spectral_graph_features2.csv
